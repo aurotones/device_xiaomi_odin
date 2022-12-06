5ae2bbf5c4c2f55c2db584512d7523d2b249d3a6
Uuganbayar <aurotones@gmail.com>
2022-12-07 01:35:27 +0800

    odin: Bring up Evolution X

--- device.mk
+++ device.mk
@@ -370,10 +370,6 @@ PRODUCT_PACKAGES += \
     libmm-omxcore \
     libstagefrighthw
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
